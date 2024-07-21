196ee0b9e6fdbb1148c169be5c16cfc7f67d9c14
Bamiwo Adebayo <bammietop@gmail.com>
2024-07-21 12:17:27 +0100

    Added Product Categories Listing API Endpoint

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,12 +4,11 @@ dotenv.config();
 const config = {
   port: process.env.PORT ?? 8000,
   "api-prefix": "api/v1",
-  // DB_USER: process.env.DB_USER,
-  // DB_HOST: process.env.DB_HOST,
-  // DB_PASSWORD: process.env.DB_PASSWORD,
-  // DB_PORT: process.env.DB_PORT,
-  // DB_NAME: process.env.DB_NAME,
-  DB_URL: process.env.DB_URL,
+  DB_USER: process.env.DB_USER,
+  DB_HOST: process.env.DB_HOST,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  DB_PORT: process.env.DB_PORT,
+  DB_NAME: process.env.DB_NAME,
 };
 
 export default config;
