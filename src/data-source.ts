ad67ab264eb659490bc833f45acc7a00e2b4b4e8
Agboola Akorede <akoredeagboola23@gmail.com>
2024-07-21 23:11:59 +0100

    fix:product server

--- src/data-source.ts
+++ src/data-source.ts
@@ -12,10 +12,10 @@ export const AppDataSource = new DataSource({
   synchronize: true,
   logging: false,
   entities: ['src/models/**/*.ts'],
-  ssl: false,
-  extra: {
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  },
+  // ssl: false,
+  // extra: {
+  //   ssl: {
+  //     rejectUnauthorized: false,
+  //   },
+  // },
 });
