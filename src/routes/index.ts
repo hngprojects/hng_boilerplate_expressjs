58d673a6015953005abf7130c0bcac4625343b22
Agboola Akorede <akoredeagboola23@gmail.com>
2024-07-22 10:52:38 +0100

    fix products

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,9 @@
 // silently ignore
-export * from "./auth";
-export * from "./user";
-export * from "./help-center";
-export * from "./testimonial";
-export * from "./sms";
-export * from "./notificationsettings";
-export * from "./job";
+export * from './auth';
+export * from './user';
+export * from './help-center';
+export * from './testimonial';
+export * from './sms';
+export * from './notificationsettings';
+export * from './job';
+export * from './product';
