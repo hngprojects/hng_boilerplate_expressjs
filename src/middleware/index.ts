53ef271eb4d3246af1173312fa2121ae83b108f4
James Ayomikun Araoye <ayomikuntemitope.araoye@gmail.com>
2024-07-22 10:18:48 +0100

    Revert "Feat/68 blog post listing"

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,4 +1,2 @@
 export * from "./error";
 export * from "./auth";
-export * from "./methodNotAllowed";
-
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,5 @@ export * from "./auth";
 export * from "./user";
 export * from "./help-center";
 export * from "./testimonial";
-export * from "./notificationsettings"
-export * from "./blog";
 export * from "./sms";
-
+export * from "./notificationsettings";
