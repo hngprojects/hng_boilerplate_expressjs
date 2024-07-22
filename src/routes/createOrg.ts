ab3fc59076f4642ee9afdb14a51d3f2bef2c30ef
PreciousIfeaka <preciousifeaka@gmail.com>
2024-07-22 10:44:56 +0100

    [FEAT]: Create Organisation resource

--- src/routes/createOrg.ts
+++ src/routes/createOrg.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
+import { authMiddleware } from "../middleware";
 import { createOrganisation } from "../controllers/createorgController"
 
 const organisationRoute = Router();
 
-organisationRoute.post("/organisations", createOrganisation);
+organisationRoute.post("/organisations", authMiddleware, createOrganisation);
 
 export { organisationRoute }
