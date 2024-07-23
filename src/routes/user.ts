a7c11e64d3cbce1203aee27d4ce986bfb9054234
CollinDex <obetscollins@gmail.com>
2024-07-23 22:23:04 +0100

    fix: Fixed Route Errors

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -5,8 +5,8 @@ import { authMiddleware } from "../middleware";
 const userRouter = Router();
 const userController = new UserController();
 
-userRouter.get("/users", userController.getAllUsers.bind(UserController));
-userRouter.delete("/users/:id", authMiddleware, userController.deleteUser.bind(userController));
+userRouter.get("/", userController.getAllUsers.bind(UserController));
+userRouter.delete("/:id", authMiddleware, userController.deleteUser.bind(userController));
 userRouter.get("/me", authMiddleware, UserController.getProfile);
 
 export { userRouter };
