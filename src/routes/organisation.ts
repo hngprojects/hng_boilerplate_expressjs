ff69322cd1cd33f1ed8f72d983a6f7c1ebbfdbe1
Fawaz Oduola <oduolafawaz@gmail.com>
2024-07-23 13:59:45 +0100

    deleted the createOrg.ts in the route folder and included the route in the organisation.ts file

--- src/routes/organisation.ts
+++ src/routes/organisation.ts
@@ -1,11 +1,17 @@
 import Router from "express";
 import { OrgController } from "../controllers/OrgController";
+import { authMiddleware } from "../middleware";
 
 const orgRouter = Router();
 const orgController = new OrgController();
 
 orgRouter.delete(
   "/organizations/:org_id/users/:user_id",
-  orgController.removeUser.bind(orgController),
+  orgController.removeUser.bind(orgController)
+);
+orgRouter.post(
+  "/organisations",
+  authMiddleware,
+  orgController.createOrganisation.bind(orgController)
 );
 export { orgRouter };
