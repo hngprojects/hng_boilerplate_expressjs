91be478d5e69d2e46299f361e8b51369f64e3332
MAC <horlarmihleykan10@gmail.com>
2024-07-23 18:20:03 +0100

    update export in admincontroller

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
-import AdminOrganisationController from "../controllers/AdminController";
+import admin from "../controllers/AdminController";
 import { authMiddleware, checkPermissions, } from "../middleware";
 import { UserRole } from "../enums/userRoles";
 
 const adminRouter = Router();
-const adminOrganisationController = new AdminOrganisationController();
+const adminOrganisationController = new admin.AdminOrganisationController();
 
 adminRouter.patch("/organisation/:id", authMiddleware, 
 checkPermissions([UserRole.SUPER_ADMIN]), 
 adminOrganisationController.updateOrg.bind(adminOrganisationController));
+
 
 export { adminRouter }; 
