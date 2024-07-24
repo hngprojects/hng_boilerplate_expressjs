b12bb4841d5e8162b17e6bf6f814eab57309607b
Simon Ugorji <ugorji757@gmail.com>
2024-07-24 16:56:16 +0100

    feat: endpoint to edit a comment should unacessible after 30 mins of creating the comment

---
+++ src/controllers/blogCommentController.ts
@@ -0,0 +1,90 @@
+import { Request, Response } from "express";
+import { editComment, createComment } from "../services/blogComment.services";
+
+export class BlogCommentController {
+  async createComment(req: Request, res: Response) {
+    const blogId = req.params.blogId;
+    const { content } = req.body;
+
+    try {
+      const comment = await createComment(blogId, content);
+      res.status(201).json({
+        status: "success",
+        status_code: 201,
+        message: "Comment created successfully.",
+        data: comment,
+      });
+    } catch (error: any) {
+      if (error.message === "Blog not found") {
+        res.status(404).json({
+          status: "unsuccessful",
+          status_code: 404,
+          message: error.message,
+        });
+      } else {
+        res.status(500).json({
+          status: "unsuccessful",
+          status_code: 500,
+          message: "Failed to create comment. Please try again later.",
+        });
+      }
+    }
+  }
+
+  async editComment(req: Request, res: Response) {
+    try {
+      //get the commentId
+      const commentId: number = Number(req?.params?.commentId || 0);
+
+      if (!commentId)
+        return res.status(403).json({
+          status: "unsuccessful",
+          message: "Invalid comment ID",
+          status_code: 403,
+        });
+
+      //get the content
+      const { content } = req.body;
+
+      if (content?.toString()?.trim() === "")
+        return res.status(403).json({
+          status: "unsuccessful",
+          message: "Content cannot be empty",
+          status_code: 403,
+        });
+
+      //call the function to edit the comment
+      const editedComment = await editComment(commentId, content as string);
+
+      //return success
+      return res.status(201).json({
+        status: "successful",
+        message: "Comment edited successfully",
+        data: editedComment,
+        status_code: 200,
+      });
+    } catch (error: any) {
+      //check error message
+      if (error.message === "COMMENT_NOT_FOUND") {
+        return res.status(404).json({
+          status: "unsuccessful",
+          message: "The comment you are trying to edit does not exist",
+          status_code: 404,
+        });
+      } else if (error.message === "TIME_NOT_OK") {
+        return res.status(403).json({
+          status: "unsuccessful",
+          message:
+            "You cannot edit or create a comment within 30 minutes of its initial creation or last update.",
+          status_code: 403,
+        });
+      } else {
+        return res.status(500).json({
+          status: "unsuccessful",
+          message: "Failed to edit comment. Please try again later.",
+          status_code: 500,
+        });
+      }
+    }
+  }
+}
--- src/routes/blog.ts
+++ src/routes/blog.ts
@@ -3,9 +3,11 @@ import { authMiddleware } from "../middleware";
 import { createBlogController } from "../controllers/createBlogController";
 import { BlogController } from "../controllers/BlogController";
 import { updateBlogController } from "../controllers/updateBlogController";
+import { BlogCommentController } from "../controllers/blogCommentController";
 
 const blogRouter = Router();
 const blogController = new BlogController();
+const blogCommentController = new BlogCommentController();
 
 blogRouter.post("/create", authMiddleware, createBlogController);
 blogRouter.get("/", blogController.listBlogs.bind(blogController));
@@ -20,7 +22,24 @@ blogRouter.get(
   authMiddleware,
   blogController.listBlogs.bind(blogController),
 );
+blogRouter.get(
+  "/",
+  authMiddleware,
+  blogController.listBlogs.bind(blogController),
+);
 
 blogRouter.delete("/:id", blogController.deleteBlogPost.bind(blogController));
+//  endpoint to create a comment on a blog post
+blogRouter.post(
+  "/:postId/comment",
+  authMiddleware,
+  blogCommentController.createComment.bind(blogCommentController),
+);
 
+//endpoint to edit a comment on a blog post
+blogRouter.post(
+  "/:commentId/comment",
+  authMiddleware,
+  blogCommentController.editComment.bind(blogCommentController),
+);
 export { blogRouter };
