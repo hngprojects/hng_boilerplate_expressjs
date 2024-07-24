f62507074a697fe2992eadf9a70262f9896469e3
Chibuikem Michael Ilonze <chibuikemichaelilonze@gmail.com>
2024-07-24 16:08:34 +0100

    feat: API Endpoint for Editing a Blog Post by Id

--- src/routes/blog.ts
+++ src/routes/blog.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import { authMiddleware } from "../middleware";
 import { createBlogController } from "../controllers/createBlogController";
 import { BlogController } from "../controllers";
+import { updateBlogController } from "../controllers/updateBlogController";
 
 const blogRouter = Router();
 const blogController = new BlogController();
 
-blogRouter.post("/create", authMiddleware, createBlogController);
+blogRouter.post("/create", createBlogController);
 blogRouter.get("/", blogController.listBlogs.bind(blogController));
+blogRouter.put("/:id", updateBlogController);
 
 export { blogRouter };
