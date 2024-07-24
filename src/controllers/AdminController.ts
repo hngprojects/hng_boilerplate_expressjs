d3d82ba0a9f6153458a055dc4e043d9f904ca7ac
PreciousIfeaka <preciousifeaka@gmail.com>
2024-07-25 00:59:05 +0100

    chore: created openapi documentation for the endpoint

--- src/controllers/AdminController.ts
+++ src/controllers/AdminController.ts
@@ -385,6 +385,84 @@ class AdminUserController {
     }
   }
 
+  /**
+   * @swagger
+   * /api/v1/admin/users/:{user-id}:
+   *   get:
+   *     summary: Superadmin - Get a single user
+   *     tags: [Admin]
+   *     parameters:
+   *       - in: path
+   *         name: user-id
+   *         required: true
+   *         description: The ID of the user data to retrieve
+   *         schema:
+   *           type: string
+   *     responses:
+   *       200:
+   *         description: User retrieved successfully
+   *         content:
+   *           application/json:
+   *             schema:
+   *               type: object
+   *               properties:
+   *                 status:
+   *                   type: string
+   *                   example: success
+   *                 data:
+   *                   type: object
+   *                   properties:
+   *                     user_id:
+   *                       type: string
+   *                     first_name:
+   *                       type: string
+   *                     last_name:
+   *                       type: string
+   *                     email:
+   *                       type: string
+   *                     phone:
+   *                       type: string
+   *                     profile_picture:
+   *                       type: string
+   *                     role:
+   *                       type: string
+   *                 status_code:
+   *                   type: integer
+   *                   example: 200
+   *       404:
+   *         description: User not found
+   *         content:
+   *           application/json:
+   *             schema:
+   *               type: object
+   *               properties:
+   *                 status:
+   *                   type: string
+   *                   example: unsuccessful
+   *                 message:
+   *                   type: string
+   *                   example: User not found
+   *                 status_code:
+   *                   type: integer
+   *                   example: 404
+   *       500:
+   *         description: Internal Server Error
+   *         content:
+   *           application/json:
+   *             schema:
+   *               type: object
+   *               properties:
+   *                 status:
+   *                   type: string
+   *                   example: error
+   *                 message:
+   *                   type: string
+   *                   example: Internal Server Error
+   *                 status_code:
+   *                   type: integer
+   *                   example: 500
+   */
+
   async getUserBySuperadmin(req: Request, res: Response): Promise<unknown> {
     const userId = req.params["user-id"];
     try {
