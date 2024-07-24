2bb08349784250ac267cb8a1356060905568c990
SHEDRACK ALITA <davisshedrack3308@gmail.com>
2024-07-24 13:31:34 +0100

    Update SmsController.ts
    Made few changes in the sms controller file

--- src/controllers/SmsController.ts
+++ src/controllers/SmsController.ts
@@ -7,20 +7,21 @@ export const sendSms = async (req: Request, res: Response): Promise<void> => {
   const { phone_number, message } = req.body;
   const sender_id = req.user.id;
 
-  if (!phone_number || !message || !sender_id) {
-    res.status(400).json({
-      status: "unsuccessful",
-      status_code: 400,
-      message:
-        "Valid phone number, message content, and sender ID must be provided.",
-    });
-    return;
-  }
-
   try {
+    //This statement checks if a phone number verified, message content and send ID is provided, and if not throw an error with status code 400
+    if (!phone_number || !message || !sender_id) {
+      res.status(400).json({
+        status: "Unsuccessful",
+        status_code: 400,
+        message:
+          "Valid phone number, message content, and sender ID must be provided.",
+      });
+      return;
+    }
+
+    //Checks if the sender id exist in the database. This implies that the send must be a registered user.
     const userRepository = AppDataSource.getRepository(User);
     const sender = await userRepository.findOneBy({ id: sender_id });
-
     if (!sender) {
       res.status(404).json({
         status: "unsuccessful",
@@ -30,6 +31,7 @@ export const sendSms = async (req: Request, res: Response): Promise<void> => {
       return;
     }
 
+    //After all coditions above are met, a success response is returned.
     await SmsService.sendSms(sender, phone_number, message);
     res.status(200).json({
       status: "success",
@@ -37,6 +39,7 @@ export const sendSms = async (req: Request, res: Response): Promise<void> => {
       message: "SMS sent successfully.",
     });
   } catch (error) {
+    //An error response is returned here if something went wrong with the Twilio API server or there is a bad network!
     res.status(500).json({
       status: "unsuccessful",
       status_code: 500,
