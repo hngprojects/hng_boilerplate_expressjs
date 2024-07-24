dfe190eefef5a6fcbc3bd66735bda8c95e8b3850
Mong Israel <mongsolomon@gmail.com>
2024-07-24 10:46:10 +0100

    added routes

--- src/routes/organisation.ts
+++ src/routes/organisation.ts
@@ -13,4 +13,8 @@ orgRouter.delete(
   "/organizations/:org_id/users/:user_id",
   orgController.removeUser.bind(orgController),
 );
+orgRouter.get(
+  "/users/:id/organizations",
+  orgController.getOrganizations.bind(orgController),
+);
 export { orgRouter };
