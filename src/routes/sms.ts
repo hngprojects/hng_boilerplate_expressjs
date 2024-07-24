aa47993cdd049836e462c0935c1b9b2ce435fa20
mwangiii <mwangi8kevin@gmail.com>
2024-07-24 20:16:11 +0300

    chore: update yarn.lock file

--- src/routes/sms.ts
+++ src/routes/sms.ts
@@ -1,17 +1,9 @@
-import { Router } from "express"
-<<<<<<< HEAD
-<<<<<<< HEAD
-import { sendSms } from "../controllers/SmsController"
-import { authMiddleware } from "../middleware"
-=======
->>>>>>> 3b86485 (feat: implement docs for product routes)
-=======
-import { sendSms } from "../controllers/SmsController"
-import { authMiddleware } from "../middleware"
->>>>>>> 26b237a (fix: resolve merge conflicts)
+import { Router } from "express";
+import { authMiddleware } from "../middleware";
+import { sendSms } from "../controllers/SmsController";
 
-const smsRouter = Router()
+const smsRouter = Router();
 
-smsRouter.post("/send", authMiddleware, sendSms)
+smsRouter.post("/send", authMiddleware, sendSms);
 
-export { smsRouter }
+export { smsRouter };
