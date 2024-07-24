f41e9f154dc3d15daa3923002e540f1639c64d92
Simon Ugorji <ugorji757@gmail.com>
2024-07-24 16:56:16 +0100

    feat: endpoint to edit a comment should unacessible after 30 mins of creating the comment

--- src/routes/blog.ts
+++ src/routes/blog.ts
@@ -2,13 +2,31 @@ import { Router } from "express";
 import { authMiddleware } from "../middleware";
 import { createBlogController } from "../controllers/createBlogController";
 import { BlogController } from "../controllers/BlogController";
+import { BlogCommentController } from "../controllers/blogCommentController";
 
 const blogRouter = Router();
 const blogController = new BlogController();
+const blogCommentController = new BlogCommentController();
 
 blogRouter.post("/create", authMiddleware, createBlogController);
-blogRouter.get("/", authMiddleware, blogController.listBlogs.bind(blogController));
+blogRouter.get(
+  "/",
+  authMiddleware,
+  blogController.listBlogs.bind(blogController),
+);
 
 blogRouter.delete("/:id", blogController.deleteBlogPost.bind(blogController));
+//  endpoint to create a comment on a blog post
+blogRouter.post(
+  "/:postId/comment",
+  authMiddleware,
+  blogCommentController.createComment.bind(blogCommentController),
+);
 
+//endpoint to edit a comment on a blog post
+blogRouter.post(
+  "/:commentId/comment",
+  authMiddleware,
+  blogCommentController.editComment.bind(blogCommentController),
+);
 export { blogRouter };
