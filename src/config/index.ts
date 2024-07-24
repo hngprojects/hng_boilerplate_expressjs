e4ddda8b783e0318f8aa6102ec144dc56b762999
SHEDRACK ALITA <davisshedrack3308@gmail.com>
2024-07-24 13:40:07 +0100

    Update index.ts
    Modified the twilio account SID dotenv variable name

--- src/config/index.ts
+++ src/config/index.ts
@@ -15,7 +15,7 @@ const config = {
   SMTP_HOST: process.env.SMTP_HOST,
   SMTP_SERVICE: process.env.SMTP_SERVICE,
   NODE_ENV: process.env.NODE_ENV,
-  TWILIO_SID: process.env.TWILIO_SID,
+  TWILIO_SID: process.env.TWILIO_ACCOUNT_SID,
   TWILIO_AUTH_TOKEN: process.env.TWILIO_AUTH_TOKEN,
   TWILIO_PHONE_NUMBER: process.env.TWILIO_PHONE_NUMBER,
 
