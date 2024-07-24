fa106b162c0e258f0a6374456f5f5fba7723c0ee
mwangiii <mwangi8kevin@gmail.com>
2024-07-24 15:04:49 +0300

    fixing conflicts

--- src/routes/sms.ts
+++ src/routes/sms.ts
@@ -1,9 +1,14 @@
 import { Router } from "express"
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { sendSms } from "../controllers/SmsController"
 import { authMiddleware } from "../middleware"
 =======
 >>>>>>> 3b86485 (feat: implement docs for product routes)
+=======
+import { sendSms } from "../controllers/SmsController"
+import { authMiddleware } from "../middleware"
+>>>>>>> 26b237a (fix: resolve merge conflicts)
 
 const smsRouter = Router()
 
