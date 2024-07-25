793cab8482046d94ea11574527a83fa370251806
Simon Ugorji <ugorji757@gmail.com>
2024-07-25 10:28:10 +0100

    Merge branch 'feat/edit-comment-unacessible-30-mins' of https://github.com/Octagon-simon/hng_boilerplate_expressjs into feat/edit-comment-unacessible-30-mins

--- src/routes/blog.ts
+++ src/routes/blog.ts
@@ -3,9 +3,11 @@ import { authMiddleware } from "../middleware";
 import { createBlogController } from "../controllers/createBlogController";
 import { BlogController } from "../controllers/BlogController";
 import { updateBlogController } from "../controllers/updateBlogController";
+import { BlogCommentController } from "../controllers/blogCommentController";
 
 const blogRouter = Router();
 const blogController = new BlogController();
+const blogCommentController = new BlogCommentController();
 
 blogRouter.post("/create", authMiddleware, createBlogController);
 blogRouter.get("/", blogController.listBlogs.bind(blogController));
@@ -22,5 +24,19 @@ blogRouter.get(
 );
 
 blogRouter.delete("/:id", blogController.deleteBlogPost.bind(blogController));
+
+//endpoint to create a comment on a blog post
+blogRouter.post(
+  "/:postId/comment",
+  authMiddleware,
+  blogCommentController.createComment.bind(blogCommentController),
+);
+
+//endpoint to edit a comment on a blog post
+blogRouter.patch(
+  "/:commentId/edit-comment",
+  authMiddleware,
+  blogCommentController.editComment.bind(blogCommentController),
+);
 
 export { blogRouter };
