a5f28ad8c913b441d697bb7faa7da2824ae88980
Adeosun Oluwaseyi <deosundeola@gmail.com>
2024-07-27 10:45:29 +0100

    feat: get user product with pagination

--- src/index.ts
+++ src/index.ts
@@ -72,7 +72,6 @@ server.use("/api/v1/product", productRouter);
 server.use("/api/v1/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 server.use("/api/v1/settings", notificationRouter);
 server.use("/api/v1/jobs", jobRouter);
-server.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 server.use("/api/v1", orgRouter);
 server.use("/api/v1", authMiddleware, orgRouter);
 server.use("/admin/queues", ServerAdapter.getRouter());
--- src/models/product.ts
+++ src/models/product.ts
@@ -1,40 +1,10 @@
-import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
-import { User } from './user';
-import ExtendedBaseEntity from './extended-base-entity';
-
-
-/**
- * @swagger
- * components:
- *   schemas:
- *     Product:
- *       type: object
- *       required:
- *         - name
- *         - description
- *         - price
- *         - category
- *       properties:
- *         id:
- *           type: string
- *           description: The auto-generated id of the product
- *         name:
- *           type: string
- *           description: Name of the product
- *         description:
- *           type: string
- *           description: Description of the product
- *         price:
- *           type: number
- *           description: Price of the product
- *         category:
- *           type: string
- *           description: Category of the product
- */
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from "typeorm";
+import { User } from "./user";
+import ExtendedBaseEntity from "./extended-base-entity";
 
 @Entity()
 export class Product extends ExtendedBaseEntity {
-  @PrimaryGeneratedColumn('uuid')
+  @PrimaryGeneratedColumn("uuid")
   id: string;
 
   @Column()
