8cb1fa6850ddfb294096797b8b82b49756f92beb
masterchief-Dave <bodunrindavidbond@gmail.com>
2024-07-29 15:36:17 +0100

    feat: implement magic link endpoint

--- src/utils/mail.ts
+++ src/utils/mail.ts
@@ -4,12 +4,10 @@ import { BadRequest } from "../middleware";
 
 const Sendmail = async (emailcontent: any) => {
   const transporter = nodemailer.createTransport({
-    // service: config.SMTP_SERVICE,
-    // host: "smtp.gmail.com",
-    // port: 587,
-    host: config.SMTP_HOST,
-    port: Number(config.SMTP_PORT),
-    // secure: false,
+    service: config.SMTP_SERVICE,
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
       user: config.SMTP_USER,
       pass: config.SMTP_PASSWORD,
