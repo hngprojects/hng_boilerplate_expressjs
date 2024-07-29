9239e1dc4018841f4798d1f357b6f22a0b9837bd
Kingsley Akpan <54294050+Khingz@users.noreply.github.com>
2024-07-29 11:12:18 +0100

    feat: modify google auth implementation

---
+++ src/controllers/GoogleAuthController.ts
@@ -0,0 +1,26 @@
+import passport from "../config/google.passport.config";
+import {ServerError, Unauthorized } from "../middleware";
+import { Request, Response, NextFunction } from "express";
+import { GoogleAuthService } from "../services/google.auth.service";
+
+export const initiateGoogleAuthRequest = passport.authenticate('google', { scope: [ 'openid', 'email', 'profile' ] })
+
+export const googleAuthCallback = (req: Request, res: Response, next: NextFunction) => {
+    const authenticate = passport.authenticate('google', async (error, user, info) => {
+        const googleAuthService = new GoogleAuthService();
+      try {
+        if (error) {            
+            throw new ServerError("Authentication error");
+        }
+        if (!user) {
+        throw new Unauthorized("Authentication failed!")
+        }
+        const isDbUser = await googleAuthService.getUserByGoogleId(user.id);
+        const dbUser = await googleAuthService.handleGoogleAuth(user, isDbUser)        
+        res.status(200).json(dbUser);
+      } catch(error) {
+        next(error)
+      }
+    });
+    authenticate(req, res, next)
+}
