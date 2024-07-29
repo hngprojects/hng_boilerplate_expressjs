92b3c53d9d160a37691fd7f7cf45020cfaa411c6
oderahub <okere_chidera@yahoo.com>
2024-07-29 14:16:59 +0100

    feat: update organization

--- src/routes/organisation.ts
+++ src/routes/organisation.ts
@@ -9,39 +9,45 @@ const orgRouter = Router();
 const orgController = new OrgController();
 
 orgRouter.get(
-  "/organisations/:org_id",
+  "/organizations/:org_id",
   authMiddleware,
   validateOrgId,
   orgController.getSingleOrg.bind(orgController),
 );
 orgRouter.delete(
-  "/organisations/:org_id/user/:user_id",
+  "/organizations/:org_id/user/:user_id",
   authMiddleware,
   validateOrgId,
   orgController.removeUser.bind(orgController),
 );
 
 orgRouter.post(
-  "/organisations",
+  "/organizations",
   authMiddleware,
   organizationValidation,
   orgController.createOrganisation.bind(orgController),
 );
 orgRouter.post(
-  "/organisations/join",
+  "/organizations/join",
   authMiddleware,
   orgController.joinOrganization.bind(orgController),
 );
 orgRouter.post(
-  "/organisations/:orgId/invite",
+  "/organizations/:orgId/invite",
   authMiddleware,
   checkPermissions([UserRole.ADMIN, UserRole.SUPER_ADMIN]),
   orgController.createInvitation.bind(orgController),
 );
 orgRouter.get(
-  "/users/:id/organisations",
+  "/users/:id/organizations",
   authMiddleware,
   orgController.getOrganizations.bind(orgController),
+);
+orgRouter.patch(
+  "/organizations/:org_id",
+  authMiddleware,
+  checkPermissions([UserRole.SUPER_ADMIN, UserRole.USER]),
+  orgController.updateOrganisation.bind(orgController),
 );
 
 export { orgRouter };
