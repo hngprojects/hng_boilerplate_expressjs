35995bbf8a233f972e55e9ff5c61a68d07062d0b
Rob-in-son <estherchidinma202@gmail.com>
2024-08-07 21:58:24 +0100

    fixes

--- init-scripts/init.sql
+++ init-scripts/init.sql
@@ -1,3 +1,14 @@
 -- init-scripts/init.sql
-CREATE USER postgres WITH PASSWORD 'postgres';
-ALTER USER postgres WITH SUPERUSER;
+\set db_user `echo "$DB_USER"`
+\set db_password `echo "$DB_PASSWORD"`
+\set db_name `echo "$DB_NAME"`
+
+CREATE USER :db_user WITH PASSWORD :'db_password';
+CREATE DATABASE :db_name;
+GRANT ALL PRIVILEGES ON DATABASE :db_name TO :db_user;
+
+\connect :db_name
+
+GRANT USAGE ON SCHEMA public TO :db_user;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO :db_user;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO :db_user;
