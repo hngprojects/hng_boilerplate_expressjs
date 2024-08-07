35959a031f1d8f89a1f23844c2ab4417c2ababc1
Rob-in-son <estherchidinma202@gmail.com>
2024-08-07 17:55:34 +0100

    fixed init-db.sh

--- init-db.sh
+++ init-db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+psql -v ON_ERROR_STOP=1 --username postgres --dbname postgres<<-EOSQL
     CREATE USER admin WITH PASSWORD 'thepassword';
     CREATE DATABASE admindb;
     GRANT ALL PRIVILEGES ON DATABASE admindb TO admin;
