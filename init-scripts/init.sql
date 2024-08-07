14f461810272d06ba94024bafce890bbeb6e9eca
Rob-in-son <estherchidinma202@gmail.com>
2024-08-07 22:54:09 +0100

    fixes

--- init-scripts/init.sql
+++ init-scripts/init.sql
@@ -1,12 +1,16 @@
+-- init-scripts/init.sql
+\set db_user `echo "$DB_USER"`
+\set db_password `echo "$DB_PASSWORD"`
+\set db_name `echo "$DB_NAME"`
+
 -- Create extensions if needed
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 -- Connect to the database
-\c ${DB_NAME}
+\c :db_name
 
 -- Grant necessary permissions
-GRANT ALL PRIVILEGES ON SCHEMA public TO ${DB_USER};
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO ${DB_USER};
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO ${DB_USER};
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON FUNCTIONS TO ${DB_USER};
-
+GRANT ALL PRIVILEGES ON SCHEMA public TO :db_user;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO :db_user;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO :db_user;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON FUNCTIONS TO :db_user;
