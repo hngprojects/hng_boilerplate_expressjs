ee2e1487e15f827789706c6d9e2b043834634f7c
Nockk <142100729+Benson-Ogheneochuko@users.noreply.github.com>
2024-08-08 19:11:48 +0100

    Update product.ts

--- src/routes/product.ts
+++ src/routes/product.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { ProductController } from "../controllers/ProductController";
-import { authMiddleware } from "../middleware";
+import { authMiddleware, validOrgAdmin } from "../middleware";
 import { validateProductDetails } from "../middleware/product";
 import { validateUserToOrg } from "../middleware/organizationValidation";
 import { adminOnly } from "../middleware";
@@ -8,20 +8,26 @@ import { adminOnly } from "../middleware";
 const productRouter = Router();
 const productController = new ProductController();
 
+// route
 productRouter.post(
   "/organizations/:org_id/products",
   validateProductDetails,
   authMiddleware,
-  adminOnly,
-  validateUserToOrg,
+  validOrgAdmin,
   productController.createProduct,
+);
+
+productRouter.get(
+  "/organizations/:org_id/products/search",
+  authMiddleware,
+  validateUserToOrg,
+  productController.getProduct,
 );
 
 productRouter.delete(
   "/organizations/:org_id/products/:product_id",
   authMiddleware,
-  adminOnly,
-  validateUserToOrg,
+  validOrgAdmin,
   productController.deleteProduct,
 );
 
