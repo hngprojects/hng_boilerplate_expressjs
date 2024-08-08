ca227de4699171087a13248b784fe30deab19a2e
Uzo-Felix <uzochukwuonyekwelu03@gmail.com>
2024-08-08 02:44:22 +0100

    feat: documentation for payment with paystack

--- src/controllers/paymentPaystackController.ts
+++ src/controllers/paymentPaystackController.ts
@@ -3,9 +3,48 @@ import { initializePayment, verifyPayment } from "../services";
 import log from "../utils/logger";
 
 /**
- * Initializes a payment using Paystack
- * @param req - Express request object
- * @param res - Express response object
+ * @swagger
+ * /payments/paystack/initiate:
+ *   post:
+ *     summary: Initiate a payment using Paystack
+ *     tags: [Payments]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 example: test@example.com
+ *               amount:
+ *                 type: number
+ *                 example: 1000
+ *               currency:
+ *                 type: string
+ *                 example: NGN
+ *     responses:
+ *       200:
+ *         description: Payment initiated successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 redirect:
+ *                   type: string
+ *                   example: https://paystack.com/redirect-url
+ *       500:
+ *         description: Error initiating payment
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   example: Error initiating payment
  */
 export const initializePaymentPaystack = async (
   req: Request,
