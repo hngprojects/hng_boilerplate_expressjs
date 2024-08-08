5ea4bb2d32103fafc2ac3a16a43ab7756b4b7d0f
Agboola Akorede <akoredeagboola23@gmail.com>
2024-08-08 14:50:04 +0100

    feat: get squeeze by id

--- src/index.ts
+++ src/index.ts
@@ -26,6 +26,7 @@ import {
   sendEmailRoute,
   testimonialRoute,
   userRouter,
+  squeezeRoute,
 } from "./routes";
 import { orgRouter } from "./routes/organisation";
 import { smsRouter } from "./routes/sms";
@@ -89,7 +90,7 @@ server.use("/api/v1", contactRouter);
 server.use("/api/v1", jobRouter);
 server.use("/api/v1", roleRouter);
 server.use("/api/v1", newsLetterSubscriptionRoute);
-
+server.use("/api/v1", squeezeRoute);
 server.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 server.use(routeNotFound);
