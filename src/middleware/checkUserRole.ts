505bbd35623aafe93ccdf477890fd87c433480d3
Nockk <ob.ogheneochuko@gmail.com>
2024-08-08 10:57:10 +0100

    feat: update product

--- src/middleware/checkUserRole.ts
+++ src/middleware/checkUserRole.ts
@@ -42,11 +42,8 @@ export const checkPermissions = (roles: UserRole[]) => {
 export function adminOnly(req: Request, res: Response, next: NextFunction) {
   const user = req.user;
 
-  if (!["super_admin", "admin"].includes(user.role.toLowerCase())) {
-    return res.status(403).json({
-      status_code: 403,
-      message: "Access denied: user not an admin",
-    });
+  if (!user || user.role !== UserRole.ADMIN) {
+    return next(new Unauthorized("Access denied. Admins only."));
   }
 
   next();
