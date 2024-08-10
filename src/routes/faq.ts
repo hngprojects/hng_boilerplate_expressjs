15e65da7caec622cc9bea4a93dcc9eb413158dd4
Oluwadara <maxegunjobi@gmail.com>
2024-08-10 12:41:12 +0100

    fix: firstcommit

--- src/routes/faq.ts
+++ src/routes/faq.ts
@@ -6,9 +6,17 @@ import { UserRole } from "../enums/userRoles";
 const faqRouter = Router();
 const faqController = new FAQController();
 
-faqRouter.post("/faqs", authMiddleware, faqController.createFAQ);
+faqRouter.post(
+  "/faqs",
+  authMiddleware,
+  checkPermissions([UserRole.SUPER_ADMIN]),
+  faqController.createFAQ,
+);
+
 faqRouter.patch("/faqs/:id", authMiddleware, faqController.updateFaq);
+
 faqRouter.get("/faqs", faqController.getFaq);
+
 faqRouter.delete(
   "/faqs/:faqId",
   authMiddleware,
